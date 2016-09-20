6a70c403523cc388120f51cff27e9a226f0c50af
TCooleen <Tman109ner@gmail.com>
2016-09-20 11:30:13 -0400

    Update PersonCreator.java

--- lab05-person-creator/PersonCreator.java
+++ lab05-person-creator/PersonCreator.java
@@ -7,47 +7,58 @@
  */
 public class PersonCreator
 {
-    // Your class begins here!
+    //The name of the person.
     private String name;
+    // The age of the person.
     private int age;
+    // The sex of the person.
     private String sex;
+    // The height of the person.
     private String height;
+    // The weight of the person.
     private String weight;
+    // The social security number of the person.
     private String social;
+    // The address of the person.
     private String address;
+    // The phone number of the person.
     private String phoneNum;
+    // The date of birth of the person.
     private String dob;
 
+    // Creates the instance of a person and records their name.
     public PersonCreator(String first, String last)
     {
     name = first + " " + last;
-}
-
-    public void showName()
-    {
-        System.out.println(name); 
     }
     
+    // Inputs the person's age.
     public void inputAge(int age1)
     {
         age = age1;
     }
     
+    // Inputs the sex of the person.
     public void inputSex (String sex1)
     {
         sex = sex1;
     }
     
+    // Input's the height of the person.
     public void inputHeight(double height1)
     {
         height = height1 + " inches";
     }
     
+    // Inputs the weight of the person.
     public void inputWeight(double weight1)
     {
         weight = weight1 + " lbs";
     }
     
+    // Inputs the social security number of the person using 3 integers.
+    // Needs to be input as a 3 digit number, then a 2 digit number, then a 4 digit number.
+    // Fails if not input in this way.
     public void inputSocial(int soc1,int soc2,int soc3)
     {
         if (100 > soc1 || soc1 > 999)
@@ -68,11 +79,15 @@ public class PersonCreator
         }
     }
     
+    // Inputs the address of the person.
     public void inputAddress(String newAdd)
     {
         address = newAdd;
     }
     
+    // Inputs the phone number of the person in 3 integers.
+    // Needs to be input as a 3 digit number, then a 3 digit number, then a 4 digit number.
+    // Fails if not input in this way.
     public void inputPhone(int areaCode, int numBegin, int numEnd)
     {
         if (100 > areaCode || areaCode > 999)
@@ -93,6 +108,11 @@ public class PersonCreator
         }
     }
     
+    // Inputs the date of birth of the person.
+    // Inputs for month are 1-12 corresponding to months of the year.
+    // Inputs for day are 1-31.
+    // Inputs for year are 1000-2100.
+    // Any other inputs will fail.
     public void inputDOB(int month, int day, int year)
     {
         if (1 > month || month > 12)
@@ -113,6 +133,7 @@ public class PersonCreator
         }
     }
     
+    // Shows all the currently collected info of this instance of person.
     public void showInfo()
     {
         System.out.println(name);
